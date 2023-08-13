f6f5524aa8e2548b2a80a2ada30645a237107e15
Google <53668973+GooglyBlox@users.noreply.github.com>
2023-08-13 15:17:19 -0700

    Update script.js

--- src/script.js
+++ src/script.js
@@ -22,3 +22,48 @@ releaseInput.addEventListener('input', () => {
 });
 
 toggleFooterText.addEventListener('change', updateApprovalTextVisibility);
+
+let pressed = false;
+let flickerInterval;
+
+function startFlickering() {
+  if (flickerInterval) {
+    clearInterval(flickerInterval);
+  }
+
+  flickerInterval = setInterval(function() {
+    if (pressed) return;
+    const downloadButton = document.getElementById('download');
+    const currentImage = downloadButton.style.backgroundImage;
+    if (currentImage.includes("downloadUnpressed.png")) {
+      downloadButton.style.backgroundImage = "url('img/downloadPressed.png')";
+    } else {
+      downloadButton.style.backgroundImage = "url('img/downloadUnpressed.png')";
+    }
+  }, 500);
+}
+startFlickering();
+
+function downloadRelease() {
+  pressed = true;
+  const downloadButton = document.getElementById('download');
+  downloadButton.style.backgroundImage = "url('img/downloadPressed.png')";
+
+  const releaseOutput = document.getElementById('releaseOutput');
+  html2canvas(releaseOutput).then(function(canvas) {
+    const link = document.createElement('a');
+    link.href = canvas.toDataURL('image/png');
+    link.download = 'release.png';
+    link.click();
+  });
+
+  setTimeout(function() {
+    downloadButton.style.backgroundImage = "url('img/downloadDone.png')";
+  }, 1000);
+
+  setTimeout(function() {
+    pressed = false;
+    downloadButton.style.backgroundImage = "url('img/downloadUnpressed.png')";
+    startFlickering();
+  }, 3000);
+}
